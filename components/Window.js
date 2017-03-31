ea59c114d7173b51450d7f2a2be058092389aa3a
Brian Doherty <bdoherty92@gmail.com>
2017-03-30 23:30:05 -0500

    starting dev

---
+++ components/Window.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Window.css';
+
+class Window extends Component {
+  render() {
+    return (
+      <div className="window">
+        
+      </div>
+    );
+  }
+}
+
+export default Window;
